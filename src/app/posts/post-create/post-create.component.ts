105738a694b3df12ca53b547a6a04f51ba52285a
RayHaha <g820324@gmail.com>
2020-10-13 21:23:10 -0400

    create the edit form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
 @Component({
@@ -8,10 +10,26 @@ import { PostsService } from '../posts.service';
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
+    private mode = 'create';
+    private postId: string;
+    private post: Post;
     
-    constructor(public postsService: PostsService){
+    constructor(public postsService: PostsService, public route: ActivatedRoute){
 
+    }
+
+    ngOnInit(){
+        this.route.paramMap.subscribe((paramMap: ParamMap) => {
+            if(paramMap.has('postId')){
+                this.mode = 'edit';
+                this.postId = paramMap.get('postId');
+                this.post = this.postsService.getPost(this.postId);
+            }else{
+                this.mode = 'create';
+                this.postId = null;
+            }
+        });
     }
 
     onAddPost(form: NgForm){
