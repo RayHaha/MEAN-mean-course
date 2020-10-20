9cb2ab0c8d46ec04558b8cba1075509480e588bb
RayHaha <g820324@gmail.com>
2020-10-20 17:58:54 -0400

    convert the form from template driven to reactive approach

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -15,12 +15,21 @@ export class PostCreateComponent implements OnInit {
     private postId: string;
     post: Post;
     isLoading = false;
+    form: FormGroup;
     
     constructor(public postsService: PostsService, public route: ActivatedRoute){
 
     }
 
     ngOnInit(){
+        this.form = new FormGroup({
+            'title': new FormControl(null, {
+                validators: [Validators.required, Validators.minLength(3)]
+            }),
+            'content': new FormControl(null, {
+                validators: [Validators.required]
+            })
+        });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if(paramMap.has('postId')){
                 this.mode = 'edit';
@@ -33,6 +42,10 @@ export class PostCreateComponent implements OnInit {
                         title: postData.title,
                         content: postData.content
                     }
+                    this.form.setValue({
+                        'title': this.post.title,
+                        'content': this.post.content
+                    });
                 });
             }else{
                 this.mode = 'create';
@@ -41,16 +54,16 @@ export class PostCreateComponent implements OnInit {
         });
     }
 
-    onSavePost(form: NgForm){
-        if(form.invalid){
+    onSavePost(){
+        if(this.form.invalid){
             return;
         }
         this.isLoading = true;
         if(this.mode === 'create'){
-            this.postsService.addPost(form.value.title, form.value.content);
+            this.postsService.addPost(this.form.value.title, this.form.value.content);
         }else{
-            this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+            this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
         }
-        form.resetForm();
+        this.form.reset();
     }
 }
