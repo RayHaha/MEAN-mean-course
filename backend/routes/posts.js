121eb9a99185535fa1c4bc262dcd287e226e52a8
RayHaha <g820324@gmail.com>
2020-11-09 22:46:30 -0500

    paginatiton backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -64,7 +64,13 @@ router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) 
 });
 
 router.get("", (req, res, next) => {
-    Post.find().then((documents) =>{
+    const pageSize = +req.query.pagesize;   // use + to change from string to number
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if(pageSize && currentPage){
+        postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+    }
+    postQuery.then((documents) =>{
         res.status(200).json({
             message: 'Posts fetched successfully!',
             posts: documents
