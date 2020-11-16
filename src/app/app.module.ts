8403acae7c6c184457b3caa150519e60bb785f79
RayHaha <g820324@gmail.com>
2020-11-16 14:34:29 -0500

    add the signup screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,15 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     PostListComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
